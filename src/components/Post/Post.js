2d12f042cd4bf4a64b95230c27442935107739a5
rezafset17 <rezafset17@gmail.com>
2020-09-07 00:07:30 +0600

    done

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,7 +6,7 @@ const Post = () => {
     const [post, setPost] = useState([]);
 
     useEffect(()=>{
-        const url = (`http://jsonplaceholder.typicode.com/posts`);
+        const url = (`https://jsonplaceholder.typicode.com/posts`);
         fetch(url)
         .then(response=>response.json())
         .then(data=>setPost(data.slice(0,18)))
